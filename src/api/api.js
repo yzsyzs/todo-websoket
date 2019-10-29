6fd011ad91e50f470a6152fc83ecde4c30a8813d
于工 <duoleimi123>
2019-10-29 16:10:45 +0800

    'master 分支, 封装了请求的 axios' of commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,67 +1,135 @@
 import axios from 'axios'
+// import { Loading, Message } from 'element-ui'
 import qs from 'qs'
 
+const getToken = () => {
+  return JSON.parse(localStorage.getItem('user'))
+}
 let baseURL = '/apis'
 // 开发环境
 if (process.env.NODE_ENV === 'development') {
   baseURL = '/apis'
 }
 // 正式环境
-if (process.env.type === 'production') {
+if (process.env.NODE_ENV === 'production') {
   baseURL = '/apis'
 }
 // 测试环境
-if (process.env.type === 'testing') {
+if (process.env.NODE_ENV === 'testing') {
   baseURL = '/apis'
 }
 axios.defaults.baseURL = baseURL
+// 请求拦截器
+axios.interceptors.request.use(
+  config => {
+    console.log(config, 'config请求拦截器')
+    //1.全局loadin配置
+    /*2.token校验:一般是在登录完成之后,将用户的token通过localStorage或者cookie存在本地;
+    然后用户每次在进入页面的时候（即在main.js中），会首先从本地存储中读取token;
+    如果token存在说明用户已经登陆过则更新vuex中的token状态;
+    然后,在每次请求接口的时候,都会在请求的header中携带token;
+    后台人员就可以根据你携带的token来判断你的登录是否过期,如果没有携带,则说明没有登录过。
+    v1.每次发送请求之前判断vuex中是否存在token
+    v2.如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录情况
+    v3.即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断*/
+    // const token = store.state.token
+    // alert(token, 'token')
+    // token && (config.headers.Authorization = token)
+    config.headers.Authorization = getToken() && getToken().accessToken
+    if (config.method == 'post') {
+      console.log('post请求统一需要做什么判断')
+    }
+    //config.headers.Accept = 'application/json'; //规定接受形式json格式
+    // showFullScreenLoading() //开启loading
+    return config
+  }, error => {
+    return Promise.reject(error)
+  })
 
-function dealResponse (response) {
-  // 登录超时或没有权限跳转到登陆
-  let st = response.data.status
-  if (st === 555) {
-    window.vm.$router.push({path: '/login'})
-    let msg = response.data.msg ? response.data.msg : '请重新登陆！'
-    window.vm.$Message.error(msg)
-    return true
-  } else {
-    return false
-  }
-}
+// 响应拦截器
+axios.interceptors.response.use(
+  response => {
+    console.log(response, 'response响应拦截器')
+    // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
+    //否则的话抛出错误
+    if (response.status === 200) {
+      // tryHideFullScreenLoading() //关闭loading
+      // Message({
+      //   showClose: true,
+      //   message: '响应成功',
+      //   type: 'success'
+      // })
+      return Promise.resolve(response)
+    } else {
+      return Promise.reject(response)
+    }
+  }, error => {
+    console.log(error, 'error')
+    if (error.response.data.status) {
+      console.log('后台错误码统一处理')
+      switch (error.response.data.status) {
+        // 401:未登录;未登录则跳转登录页面，并携带当前页面的路径;在登录成功后返回当前页面，这一步需要在登录页操作。
+        case 401:
+          // router.replace({
+          //   path: '/login',
+          //   query: {
+          //     redirect: router.currentRoute.fullPath
+          //   }
+          // })
+          break
+        // 403:token过期;登录过期对用户进行提示;清除本地token和清空vuex中token对象;跳转登录页面
+        case 403:
+          // Message({
+          //   showClose: true,
+          //   message: '登录过期，请重新登录',
+          //   duration: 1000,
+          //   type: 'warning'
+          // })
+          //清除token
+          localStorage.removeItem('user')
+          //跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
+          // setTimeout(() => {
+          //   router.replace({
+          //     path: '/login',
+          //     query: {
+          //       redirect: router.currentRoute.fullPath
+          //     }
+          //   })
+          // }, 1000)
+          break
+        //404请求不存在
+        case 404:
+          // Message({
+          //   showClose: true,
+          //   message: '网络请求不存在',
+          //   duration: 1000,
+          //   type: 'error'
+          // })
+          break
+        //其他错误,直接抛出错误提示
+        default:
+          // Message({
+          //   showClose: true,
+          //   message: error.response.data.message,
+          //   duration: 1000,
+          //   type: 'error'
+          // })
+      }
+    }
+    return Promise.reject(error)
+  })
 
-// 给post 添加 中断请求
-window.sourceData = []
-
-function myAxios (option) {
+const myAxios = option => {
   // axios post 提交数据，后台获取不到提交的数据解决方案
   if (option.headers === undefined && option.method === 'POST' && option.data) {
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
     // 坑，qs.stringify处理的对象中，key对应的值不能有json字符串（对象or数组）
     option.data = qs.stringify(option.data)
   }
-  // 避免数组数据太多
-  if (window.sourceData.length > 300) {
-    window.sourceData = window.sourceData.slice(200, window.sourceData.length)
-  }
-  let source = axios.CancelToken.source()
-  window.sourceData.push(source)
-  option.cancelToken = source.token
-  return axios(option).then(function (response) {
-    if (dealResponse(response)) {
-      return
-    }
-    return Promise.resolve(response)
-  }).catch(function (error) {
-    if (error.message === '取消请求') {
-      // 中断请求返回一个 promise
-      return new Promise((resolve, reject) => {
-        resolve({status: 999})
-      })
-    }
-    if (dealResponse(error.response)) {
-      return
-    }
-    return Promise.reject(error)
+  return axios(option).then(res => {
+    return Promise.resolve(res.data)
+  }).catch(err => {
+    return Promise.reject(err.message)
   })
 }
 
@@ -75,9 +143,24 @@ let api = {
     let option = {method: 'POST', url: url, data: data, responseType: 'blob'}
     return myAxios(option)
   },
+  do_gett: function (url, data) {
+    // 通用 get 接口
+    let option = {method: 'GET', url: url, params: data}
+    return myAxios(option)
+  },
   do_put: function (url, data) {
     // 通用 post 接口
     let option = {method: 'PUT', url: url, data: data}
+    return myAxios(option)
+  },
+  do_del: function (url, data) {
+    // 通用 删除 调用
+    let option = {
+      method: 'DELETE',
+      url: url,
+      data: JSON.stringify(data),
+      headers: {'Content-Type': 'application/json;charset=UTF-8'}
+    }
     return myAxios(option)
   },
   put_post: function (url, data, method) {
